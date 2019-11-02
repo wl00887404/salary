3de83bf7ca6c9cd80a9bb72ca7412eb1d2e7a7ed
wl00887404 <wl00887404@gmail.com>
2019-11-02 15:34:21 +0800

    2019/11/2

--- index.js
+++ index.js
@@ -1,48 +1,60 @@
-const { writeFileSync } = require("fs");
+const { readFileSync, writeFileSync } = require("fs");
 
-const main = cohort => {
-  // 1/4
-  const comments = require(`./comments${cohort}.json`);
+const readYaml = path =>
+  readFileSync(path, { encoding: "utf-8" })
+    .split("\n")
+    .map(raw => new Date(raw))
+    .map(date => `2019-${date.getMonth() + 1}-${date.getDate()}`);
 
-  // 1/6
-  const assignments = require(`./assignment${cohort}.json`);
+// 1/4
+const comments = readYaml("./comments.yaml");
 
-  const result = new Map();
+// 1/6
+const assignments = readYaml("./assignments.yaml");
 
-  comments.forEach(comment => {
-    const value = result.get(comment) || 0;
-    result.set(comment, value + 1 / 4);
-  });
+const commentsSet = new Set(comments);
+const assignmentsSet = new Set(assignments);
+const makeEvent = key => {
+  const events = [];
 
-  assignments.forEach(assignment => {
-    const value = result.get(assignment) || 0;
-    result.set(assignment, value + 1 / 6);
-  });
+  if (commentsSet.has(key)) events.push("回復問題");
+  if (assignmentsSet.has(key)) events.push("回復作業問題");
 
-  result.forEach((value, key) => {
-    result.set(key, Math.ceil(value / 0.25) * 0.25); // 不滿 0.25 取 0.25
-  });
-
-  let sum = 0;
-  result.forEach(value => {
-    sum += value;
-  });
-
-  let csv = Array.from(result)
-    .sort(
-      ([key1], [key2]) => new Date(key1).getTime() - new Date(key2).getTime()
-    )
-    .map(([key, value]) => {
-      const date = new Date(key);
-      return `${date.getMonth() + 1}, ${date.getDate()}, ${value}`;
-    })
-    .join("\n");
-
-  writeFileSync(
-    `./result${cohort}.csv`,
-    "月, 日, 時數\n" + csv + `\n, 總和, ${sum}`
-  );
+  return events.join(" / ");
 };
 
-main(2);
-main(3);
+const result = new Map();
+
+comments.forEach(comment => {
+  const value = result.get(comment) || 0;
+  result.set(comment, value + 1 / 4);
+});
+
+assignments.forEach(assignment => {
+  const value = result.get(assignment) || 0;
+  result.set(assignment, value + 1 / 6);
+});
+
+result.forEach((value, key) => {
+  result.set(key, Math.ceil(value / 0.25) * 0.25); // 不滿 0.25 取 0.25
+});
+
+let sum = 0;
+result.forEach(value => {
+  sum += value;
+});
+
+let csv = Array.from(result)
+  .sort(([key1], [key2]) => new Date(key1).getTime() - new Date(key2).getTime())
+  .map(([key, value]) => `振志, INTRO, ${makeEvent(key)}, ${key}, ${value}`)
+  .join("\n");
+
+const headers = [
+  "姓名",
+  "學期",
+  "Event / Support",
+  "日期",
+  "時數(最小單位：0.25 小時)"
+];
+
+writeFileSync(`./result.csv`, `${headers.join(", ")}\n${csv}\n總和, ${sum}`);
