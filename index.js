052f355536e9cb548bfe253e7c62d35d2a297847
wl00887404 <wl00887404@gmail.com>
2019-11-05 17:17:12 +0800

    學期一是 1/10 你要先講

--- index.js
+++ index.js
@@ -1,38 +1,41 @@
-const { readFileSync, writeFileSync } = require("fs");
+const { readFileSync, writeFileSync } = require('fs');
 
 const readYaml = path =>
-  readFileSync(path, { encoding: "utf-8" })
-    .split("\n")
+  readFileSync(path, { encoding: 'utf-8' })
+    .split('\n')
     .map(raw => new Date(raw))
     .map(date => `2019-${date.getMonth() + 1}-${date.getDate()}`);
 
 // 1/4
-const comments = readYaml("./comments.yaml");
+const hoursPerComment = 1 / 4;
+const comments = readYaml('./comments.yaml');
 
 // 1/6
-const assignments = readYaml("./assignments.yaml");
+// 學期一是 1/10 你要先講
+const hoursPerAssignment = 1 / 10;
+const assignments = readYaml('./assignments.yaml');
 
 const commentsSet = new Set(comments);
 const assignmentsSet = new Set(assignments);
 const makeEvent = key => {
   const events = [];
 
-  if (commentsSet.has(key)) events.push("回復問題");
-  if (assignmentsSet.has(key)) events.push("回復作業問題");
+  if (commentsSet.has(key)) events.push('回復問題');
+  if (assignmentsSet.has(key)) events.push('回復作業問題');
 
-  return events.join(" / ");
+  return events.join(' / ');
 };
 
 const result = new Map();
 
 comments.forEach(comment => {
   const value = result.get(comment) || 0;
-  result.set(comment, value + 1 / 4);
+  result.set(comment, value + hoursPerComment);
 });
 
 assignments.forEach(assignment => {
   const value = result.get(assignment) || 0;
-  result.set(assignment, value + 1 / 6);
+  result.set(assignment, value + hoursPerAssignment);
 });
 
 result.forEach((value, key) => {
@@ -47,14 +50,14 @@ result.forEach(value => {
 let csv = Array.from(result)
   .sort(([key1], [key2]) => new Date(key1).getTime() - new Date(key2).getTime())
   .map(([key, value]) => `振志, INTRO, ${makeEvent(key)}, ${key}, ${value}`)
-  .join("\n");
+  .join('\n');
 
 const headers = [
-  "姓名",
-  "學期",
-  "Event / Support",
-  "日期",
-  "時數(最小單位：0.25 小時)"
+  '姓名',
+  '學期',
+  'Event / Support',
+  '日期',
+  '時數(最小單位：0.25 小時)'
 ];
 
-writeFileSync(`./result.csv`, `${headers.join(", ")}\n${csv}\n總和, ${sum}`);
+writeFileSync(`./result.csv`, `${headers.join(', ')}\n${csv}\n總和, ${sum}`);
