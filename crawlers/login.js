faea4c1a0d8bf054f6dbecb7cd48e5faf0c9bc11
Max He <wl00887404@gmail.com>
2020-08-06 10:40:58 +0800

    remove private message from crawlers/login

--- crawlers/login.js
+++ crawlers/login.js
@@ -1,12 +1,13 @@
 const puppeteer = require('puppeteer');
+const { account, password } = require('../auth.json');
 
 const login = async headless => {
   const browser = await puppeteer.launch({ headless });
   const page = (await browser.pages())[0];
 
   await page.goto('https://lighthouse.alphacamp.co/users/sign_in');
-  await page.type('#user_email', 'wl00887404@gmail.com');
-  await page.type('#user_password', 'u4f55u632fu5fd7');
+  await page.type('#user_email', account);
+  await page.type('#user_password', password);
   page.click('input[value="登入"]');
   await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
 
